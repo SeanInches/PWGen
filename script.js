62903a20e0af69e29cf7a0d5de85038652fc4921
Sean <seanminches@gmail.com>
2021-11-30 13:08:22 -0500

    finally working

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ var generateBtn = document.querySelector("#generate");
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var upperRandom = upperCase[Math.floor(Math.random() * upperCase.length)];
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var lowerRandom = lowerCase[Math.floor(math.random() * upperCase.length)];
+var lowerRandom = lowerCase[Math.floor(Math.random() * upperCase.length)];
 var numbers = "1234567890";
-var numberRandom = numbers[math.floor(math.random() * numbers.length)];
+var numberRandom = numbers[Math.floor(Math.random() * numbers.length)];
 var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var symbolsRandom = symbols[math.floor(math.random() * symbols.length)];
+var symbolsRandom = symbols[Math.floor(Math.random() * symbols.length)];
 
 // Write password to the #password input
 function randomCharacter() {
@@ -42,7 +42,7 @@ function generatePassword() {
   var symbolConfirm = confirm("Would you like to include special characters?")
   if (symbolConfirm === true) {
     finalPassword += symbolsRandom
-    allPossibleChar = allPossibleChar+symbols;
+    allPossibleChar = allPossibleChar + symbols;
   };
   if (allPossibleChar.length == 0){
     alert("Select one character type");
